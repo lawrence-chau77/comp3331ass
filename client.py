eb836312f29377c8986b13e88d56a133b432c376
Lawrence Chau <lawrence.chau77@gmail.com>
2022-08-03 04:11:13 +1000

    BCM works...

--- client.py
+++ client.py
@@ -1,53 +1,73 @@
 from datetime import datetime
+import json
 from socket import *
 import sys
 
 # attemps to authenticate the user
-def attemptLogin(clientSocket, udp): 
-    username = input("Username: ")
-    password = input("Password: ")
+def attemptLogin(username, password, clientSocket, udp): 
     message = f'{{"type": "login", "username": "{username}", "password": "{password}"}}' 
     clientSocket.sendall(message.encode())
     while True:
         data = clientSocket.recv(1024)
-        receivedMessage = data.decode()
+        receivedMessage = json.loads(data.decode()) 
         # if successful send details and break 
-        if receivedMessage == "success":
+        if receivedMessage['type'] == "success":
             print("Welcome to TOOM!")
             timeStamp = datetime.now().strftime("%d %B %Y %H:%M:%S")
-            loginDetails = f'{{"type": "login", "timestamp": "{timeStamp}" ,"username": "{username}", "udp": "{udp}"}}' 
-            print("Enter one of the following commands (BCM, ATU, SRB, SRM, RDM, OUT): ")
+            loginDetails = f'{{"type": "authenticated", "timestamp": "{timeStamp}" ,"username": "{username}", "udp": "{udp}"}}' 
             clientSocket.sendall(loginDetails.encode())
             break
         # if fail ask for password again and send to server
-        elif receivedMessage == "fail":
+        elif receivedMessage['type'] == "fail":
             print("Invalid password. Please try again")
             password = input("Password: ")
             clientSocket.sendall(message.encode())
             continue
         # if timeout end terminal
-        elif receivedMessage == "timeout":
+        elif receivedMessage['type'] == "timeout":
             print("Invalid pasword. Your account has been blocked. Please try again later")
-            break
+            quit()
         # if account blocked end terminal 
-        elif receivedMessage == "blocked":
+        elif receivedMessage['type'] == "blocked":
             print("Your account is blocked due to multiple login failures. Please try again later")
-            break
-
-def authenticate(ip, port, udpPort) :
-    serverAddress = ip 
-    serverPort = port 
-    clientSocket = socket(AF_INET, SOCK_STREAM)
-    serverAddress = (serverAddress, serverPort)
-    clientSocket.connect(serverAddress)
-    attemptLogin(clientSocket, udpPort)
-    # while true:
+            quit()
             
 if __name__ == '__main__':
     if len(sys.argv) < 4:
         print('required server ip, server port and udp port')
         exit(1)
-    ip =  (sys.argv[1])
-    port = int (sys.argv[2])
+    serverAddress =  (sys.argv[1])
+    serverPort = int (sys.argv[2])
     udpPort = int (sys.argv[3])
-    authenticate(ip, port, udpPort)
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    serverAddress = (serverAddress, serverPort)
+    clientSocket.connect(serverAddress)
+    username = input("Username: ")
+    password = input("Password: ")
+    attemptLogin(username, password, clientSocket, udpPort)
+    while True:
+        commandArgs = input("Enter one of the following commands (BCM, ATU, SRB, SRM, RDM, OUT) : ")
+        command = commandArgs.split()
+        if (command[0] == "BCM"):
+            broadcast = commandArgs[commandArgs.index(' ') + 1:]
+            timeStamp = datetime.now().strftime("%d %B %Y %H:%M:%S")
+            message = f'{{"type": "BCM", "timestamp": "{timeStamp}", "username": "{username}", "message": "{broadcast}"}}' 
+            clientSocket.sendall(message.encode())
+            data = clientSocket.recv(1024)
+            receivedMessage = json.loads(data.decode())
+            if receivedMessage["type"] == "bcm":
+                messageNumber = receivedMessage["messageNumber"]
+                timestamp = receivedMessage["timestamp"]
+                print(f"Broadcast message, #{messageNumber} broadcast at {timestamp}")
+                continue
+        elif (command[0] == "ATU"):
+            print("ATU")
+        elif (command[0] == "SRB"):
+            print("SRB")
+        elif (command[0] == "SRM"):
+            print("SRM")
+        elif (command[0] == "RDM"):
+            print("RDM")
+        elif (command[0] == "OUT"):
+            print("OUT")
+        
--- server.py
+++ server.py
@@ -3,20 +3,18 @@ import json
 from socket import *
 from sqlite3 import connect
 import sys
+import os
 from threading import Thread 
 
 blockedUsers = {}
 loginAttempts = {}
+activeUsers = {}
 
-def server(port, numFails) :
-    serverPort = port 
-    serverSocket = socket(AF_INET, SOCK_STREAM)
-    serverSocket.bind(('localhost', serverPort))
-    serverSocket.listen(1)
-    print("Server is on")
-    while (True): 
-        connectionSocket, addr = serverSocket.accept()
-        
+def createFiles():
+    f = open("userlog.txt", "w")
+    f.close()
+    f = open("messagelog.txt", "w")
+    f.close()
                 
 class ClientThread(Thread):
     def __init__(self, clientAddress, clientSocket, numFails):
@@ -34,6 +32,7 @@ class ClientThread(Thread):
             if not data:
                 break
             receivedMessage = json.loads(data.decode()) 
+            print(receivedMessage)
             if (receivedMessage['type'] == "login"):
                 user = receivedMessage['username']
                 pw = receivedMessage['password']
@@ -45,35 +44,67 @@ class ClientThread(Thread):
                     if (duration.seconds >= 10):
                         blockedUsers.pop(user)
                     else:
-                        self.clientSocket.send(("blocked").encode())
+                        message = f'{{"type": "blocked"}}' 
+                        self.clientSocket.send((message).encode())
                         continue
                 # check if password and username are correct
                 # if correct send success message otherwise add user to failed attempts
-                for line in open("credentials.txt", "r").readlines():
-                    login = line.split()
-                    if user == login[0] and pw == login[1]:
-                        print("Successfully logged in")
-                        message = "success"
-                        self.clientSocket.send(message.encode())
-                        print("continued")
-                        continue
+                elif (user not in blockedUsers):
+                    for line in open("credentials.txt", "r").readlines():
+                        login = line.split()
+                        if user == login[0] and pw == login[1]:
+                            message = f'{{"type": "success"}}' 
+                            self.clientSocket.send((message).encode())
+                            continue
                 # if failed before add fail attempt
-                if user in loginAttempts: 
+                elif user in loginAttempts: 
                     loginAttempts[user] += 1
                     # if fail attempts exceed numFails add to blocked users and send timeout 
                     if (loginAttempts[user] >= self.numFails):
                         blockedUsers[user] = datetime.now()
-                        self.clientSocket.send(("timeout").encode())
+                        message = f'{{"type": "timeout"}}' 
+                        self.clientSocket.send((message).encode())
                         continue
                 # add user has failed an attempt 
                 else:
                     loginAttempts[user] = 1
-                self.clientSocket.send(("fail").encode())
+                    message = f'{{"type": "fail"}}' 
+                    self.clientSocket.send((message).encode())
                 
-            elif (receivedMessage['type'] == "login"):
+            elif (receivedMessage['type'] == "authenticated"):
                 # set address and sequence number make the document for active users 
-                print("yep")
-    
+                sequenceNumber = len(activeUsers) + 1
+                timestamp = receivedMessage["timestamp"]
+                user = receivedMessage["username"]
+                addr = self.clientAddress
+                udp = receivedMessage["udp"]
+                activeUsers[sequenceNumber] = user
+                    #append
+                f = open("userlog.txt", "a")
+                f.write(f"{sequenceNumber}; {timestamp}; {user}; {addr[0]}; {udp}")
+                f.write("\n")
+                f.close()
+            
+            elif (receivedMessage['type'] == "BCM"):
+                f = open("messagelog.txt", "r")
+                messageNumber = len(f.readlines()) + 1
+                f.close()
+                timestamp = receivedMessage["timestamp"]
+                user = receivedMessage["username"]
+                message = receivedMessage["message"]
+                f = open("messagelog.txt", "a")
+                f.write(f"{messageNumber}; {timestamp}; {user}; {message}")
+                f.write("\n")
+                f.close()
+                print(f'{user} broadcasted BCM #{messageNumber} "{message}" at {timestamp}')
+                message = f'{{"type": "bcm", "messageNumber": "{messageNumber}", "timestamp": "{timestamp}"}}' 
+                self.clientSocket.send((message).encode())
+        
+        
+        self.clientSocket.close()
+        os.remove("credentials.txt")
+        os.remove("userlog.txt")
+        
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
@@ -88,6 +119,7 @@ if __name__ == '__main__':
     serverSocket = socket(AF_INET, SOCK_STREAM)
     serverSocket.bind(('localhost', serverPort))
     print("Server is on")
+    createFiles()
     while True:
         serverSocket.listen()
         connectionSocket, addr = serverSocket.accept()
