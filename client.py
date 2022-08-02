d745d7df10405b9c4a97142213e9dec934893e00
Lawrence Chau <lawrence.chau77@gmail.com>
2022-08-02 23:02:11 +1000

    Added multithreading

--- client.py
+++ client.py
@@ -1,8 +1,8 @@
 from datetime import datetime
-import json
 from socket import *
 import sys
 
+# attemps to authenticate the user
 def attemptLogin(clientSocket, udp): 
     username = input("Username: ")
     password = input("Password: ")
@@ -11,21 +11,25 @@ def attemptLogin(clientSocket, udp):
     while True:
         data = clientSocket.recv(1024)
         receivedMessage = data.decode()
+        # if successful send details and break 
         if receivedMessage == "success":
             print("Welcome to TOOM!")
-            timeStamp = datetime.strftime("%d %B %Y %H:%M:%S")
+            timeStamp = datetime.now().strftime("%d %B %Y %H:%M:%S")
             loginDetails = f'{{"type": "login", "timestamp": "{timeStamp}" ,"username": "{username}", "udp": "{udp}"}}' 
             print("Enter one of the following commands (BCM, ATU, SRB, SRM, RDM, OUT): ")
             clientSocket.sendall(loginDetails.encode())
             break
+        # if fail ask for password again and send to server
         elif receivedMessage == "fail":
             print("Invalid password. Please try again")
             password = input("Password: ")
             clientSocket.sendall(message.encode())
             continue
+        # if timeout end terminal
         elif receivedMessage == "timeout":
             print("Invalid pasword. Your account has been blocked. Please try again later")
             break
+        # if account blocked end terminal 
         elif receivedMessage == "blocked":
             print("Your account is blocked due to multiple login failures. Please try again later")
             break
@@ -37,6 +41,7 @@ def authenticate(ip, port, udpPort) :
     serverAddress = (serverAddress, serverPort)
     clientSocket.connect(serverAddress)
     attemptLogin(clientSocket, udpPort)
+    # while true:
             
 if __name__ == '__main__':
     if len(sys.argv) < 4:
--- server.py
+++ server.py
@@ -2,7 +2,8 @@ from datetime import datetime
 import json
 from socket import *
 from sqlite3 import connect
-import sys 
+import sys
+from threading import Thread 
 
 blockedUsers = {}
 loginAttempts = {}
@@ -15,9 +16,21 @@ def server(port, numFails) :
     print("Server is on")
     while (True): 
         connectionSocket, addr = serverSocket.accept()
-        while (True):
-            # recieve data 
-            data = connectionSocket.recv(1024)
+        
+                
+class ClientThread(Thread):
+    def __init__(self, clientAddress, clientSocket, numFails):
+        Thread.__init__(self)
+        self.clientAddress = clientAddress
+        self.clientSocket = clientSocket
+        self.clientAlive = False 
+        self.numFails = numFails
+        print(" New connection created for: ", clientAddress)
+        self.clientAlive = True 
+    
+    def run(self):
+        while self.clientAlive:
+            data = self.clientSocket.recv(1024)
             if not data:
                 break
             receivedMessage = json.loads(data.decode()) 
@@ -32,7 +45,7 @@ def server(port, numFails) :
                     if (duration.seconds >= 10):
                         blockedUsers.pop(user)
                     else:
-                        connectionSocket.send(("blocked").encode())
+                        self.clientSocket.send(("blocked").encode())
                         continue
                 # check if password and username are correct
                 # if correct send success message otherwise add user to failed attempts
@@ -41,39 +54,42 @@ def server(port, numFails) :
                     if user == login[0] and pw == login[1]:
                         print("Successfully logged in")
                         message = "success"
-                        connectionSocket.send(message.encode())
+                        self.clientSocket.send(message.encode())
                         print("continued")
                         continue
                 # if failed before add fail attempt
                 if user in loginAttempts: 
                     loginAttempts[user] += 1
                     # if fail attempts exceed numFails add to blocked users and send timeout 
-                    if (loginAttempts[user] >= numFails):
+                    if (loginAttempts[user] >= self.numFails):
                         blockedUsers[user] = datetime.now()
-                        connectionSocket.send(("timeout").encode())
+                        self.clientSocket.send(("timeout").encode())
                         continue
                 # add user has failed an attempt 
                 else:
                     loginAttempts[user] = 1
-                connectionSocket.send(("fail").encode())
+                self.clientSocket.send(("fail").encode())
                 
             elif (receivedMessage['type'] == "login"):
                 # set address and sequence number make the document for active users 
                 print("yep")
-                
-            
-        
     
-    
+
 if __name__ == '__main__':
     if len(sys.argv) != 3:
         print("Required server_port and number_of_consecutive_failed_attemps")
         exit()
-    port = int(sys.argv[1])
-    num_fail = int(sys.argv[2])
-    if (1 >= num_fail and num_fail >= 5):    
+    serverPort = int(sys.argv[1])
+    numFails = int(sys.argv[2])
+    if (1 > numFails and numFails > 5):    
         print("Has to be an integer between 1 and 5")
         print(sys.argv[2])
         exit()
-    num_fails = int(sys.argv[2])
-    server(port, num_fails)
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    serverSocket.bind(('localhost', serverPort))
+    print("Server is on")
+    while True:
+        serverSocket.listen()
+        connectionSocket, addr = serverSocket.accept()
+        clientThread = ClientThread(addr, connectionSocket, numFails)
+        clientThread.start()
