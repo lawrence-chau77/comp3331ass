590d9598074fc9378f66cdd68fc806f4c69eb9c5
Lawrence Chau <lawrence.chau77@gmail.com>
2022-08-04 23:30:49 +1000

    Implemented SRB and fixed authentication

--- client.py
+++ client.py
@@ -11,6 +11,7 @@ def attemptLogin(username, password, clientSocket, udp):
     while True:
         data = clientSocket.recv(1024)
         receivedMessage = json.loads(data.decode()) 
+        print(receivedMessage)
         # if successful send details and break 
         if receivedMessage['type'] == "success":
             print("Welcome to TOOM!")
@@ -22,6 +23,7 @@ def attemptLogin(username, password, clientSocket, udp):
         elif receivedMessage['type'] == "fail":
             print("Invalid password. Please try again")
             password = input("Password: ")
+            message = f'{{"type": "login", "username": "{username}", "password": "{password}"}}' 
             clientSocket.sendall(message.encode())
             continue
         # if timeout end terminal
@@ -86,9 +88,51 @@ if __name__ == '__main__':
                     receivedMessage = json.loads(data.decode())
                     
         elif (command[0] == "SRB"):
-            print("SRB")
+            if len(command) == 1:
+                print("Error. Usernames are required for SRB command")
+                continue
+            usernames = commandArgs[commandArgs.index(' ') + 1:]
+            message = f'{{"type": "SRB", "username": "{username}", "users": "{usernames}"}}'
+            clientSocket.sendall(message.encode())
+            data = clientSocket.recv(1024)
+            receivedMessage = json.loads(data.decode())
+            # if user doesn't exist
+            if (receivedMessage["type"] == "invalid"):
+                print(f'{receivedMessage["user"]} does not exist')
+                continue
+            # if user is offline 
+            elif (receivedMessage["type"] == "inactive"):
+                print(f'{receivedMessage["user"]} is offline')
+                continue
+            # if user is part of names
+            elif (receivedMessage["type"] == "itself"):
+                print(f'Cannot include yourself in separate room')
+                continue
+            # if room already exists
+            elif (receivedMessage["type"] == "roomExists"):
+                print(f'A separate room (ID: {receivedMessage["id"]}) already created for these users')
+                continue
+            # successfully created
+            elif (receivedMessage["type"] == "srb"):
+                print(f'Separate chat room has been created, room ID: {receivedMessage["id"]}, users in this room: {receivedMessage["users"]}')
+                
         elif (command[0] == "SRM"):
-            print("SRM")
+            if len(command) == 1:
+                print("roomID and message is required for SRM command")
+            roomId = command[1]
+            broadcast = ' '.join(command[2:])
+            message = f'{{"type": "SRM", "id": "{roomId}", "username": "{username}", "message": "{broadcast}"}}'
+            clientSocket.sendall(message.encode())
+            data = clientSocket.recv(1024)
+            receivedMessage = json.loads(data.decode())
+            # if room id doesn't exist
+            if (receivedMessage["type"] == "invalidRoom"):
+                print("The separate room does not exist")
+                continue
+            elif (receivedMessage["type"] == "invalidMember"):
+                print("You are not in this separate room chat")
+                continue
+            
         elif (command[0] == "RDM"):
             print("RDM")
         elif (command[0] == "OUT"):
--- server.py
+++ server.py
@@ -6,16 +6,44 @@ import sys
 import os
 from threading import Thread 
 
+# dict containing blocked users, value is datetime of when user got blocked
 blockedUsers = {}
+# dict containing each users login attemps, value is login attempts
 loginAttempts = {}
+# dict containing active users, value is sequence number
 activeUsers = {}
+# dict containing seperate rooms, value is users in each room 
+seperateRooms = {}
 
 def createFiles():
     f = open("userlog.txt", "w")
     f.close()
     f = open("messagelog.txt", "w")
     f.close()
-                
+
+def checkCredentials(member):
+    with open("credentials.txt", "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            check = line.split()
+            if member == check[0]:
+                return True
+        return False
+
+def checkActive(member):
+    with open("userlog.txt", "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            check = line.split("; ")
+            if member == check[2]:
+                return True
+        return False
+
+def checkRooms(v, chatMembers):
+    if len(v) != len(chatMembers):
+        return False
+    return sorted(v) == sorted(chatMembers)
+    
 class ClientThread(Thread):
     def __init__(self, clientAddress, clientSocket, numFails):
         Thread.__init__(self)
@@ -49,15 +77,15 @@ class ClientThread(Thread):
                         continue
                 # check if password and username are correct
                 # if correct send success message otherwise add user to failed attempts
-                elif (user not in blockedUsers):
+                if (user not in blockedUsers):
                     for line in open("credentials.txt", "r").readlines():
                         login = line.split()
                         if user == login[0] and pw == login[1]:
                             message = f'{{"type": "success"}}' 
                             self.clientSocket.send((message).encode())
                             continue
-                # if failed before add fail attempt
-                elif user in loginAttempts: 
+                # if failed before add fail attempt)
+                if user in loginAttempts: 
                     loginAttempts[user] += 1
                     # if fail attempts exceed numFails add to blocked users and send timeout 
                     if (loginAttempts[user] >= self.numFails):
@@ -65,6 +93,9 @@ class ClientThread(Thread):
                         message = f'{{"type": "timeout"}}' 
                         self.clientSocket.send((message).encode())
                         continue
+                    else:
+                        message = f'{{"type": "fail"}}' 
+                    self.clientSocket.send((message).encode())
                 # add user has failed an attempt 
                 else:
                     loginAttempts[user] = 1
@@ -131,14 +162,14 @@ class ClientThread(Thread):
                 #remove user from active users
                 user = receivedMessage["username"]
                 remove = activeUsers.pop(user)
-                #every sequence number higher than number remove 1 
+                #every sequence number higher than user sequence number remove 1 
                 for k, v in activeUsers.items():
                     if v > remove:
                         activeUsers[k] = (v-1)
                         
-                # remove line with user
                 with open("userlog.txt", "r") as f:
                     lines = f.readlines()
+                # override with every line except the one containing user
                 with open("userlog.txt", "w") as f:
                     for line in lines:
                         if receivedMessage["username"] not in line:
@@ -153,7 +184,89 @@ class ClientThread(Thread):
                 message = f'{{"type": "out", "username": "{receivedMessage["username"]}"}}'
                 self.clientSocket.send((message).encode())
                 print(f'{receivedMessage["username"]} logout')
-                
+            
+            elif (receivedMessage['type'] == "SRB"):
+                usernames = receivedMessage["users"]
+                chatMembers = usernames.split(" ")
+                # check all usernames exist from credentials
+                invalid = False
+                for member in chatMembers:
+                    # if not return doesn't exist
+                    if (not checkCredentials(member)):
+                        message = f'{{"type": "invalid", "user": "{member}"}}'
+                        self.clientSocket.send((message).encode())
+                        invalid = True
+                        break
+                if invalid:
+                    continue
+                # check all usernames online from userlog
+                inactive = False
+                for member in chatMembers:
+                    # if not return is offline
+                    if (not checkActive(member)):
+                        message = f'{{"type": "inactive", "user": "{member}"}}'
+                        print("sent inactive")
+                        self.clientSocket.send((message).encode())
+                        inactive = True
+                        break
+                if inactive:
+                    continue
+                itself = False
+                # check user isn't in names
+                for member in chatMembers:
+                    if member == receivedMessage["username"]:
+                        message = f'{{"type": "itself", "user": "{member}"}}'
+                        self.clientSocket.send((message).encode())
+                        itself = True
+                        break
+                if itself:
+                    continue    
+                roomExists = False
+                # check room doesn't already exist
+                for k,v in seperateRooms.items():
+                    if checkRooms(v, chatMembers):
+                        message = f'{{"type": "roomExists", "id": "{k}"}}'
+                        self.clientSocket.send((message).encode())
+                        roomExists = True
+                        break
+                if roomExists:
+                    continue
+                # create room
+                roomId = len(seperateRooms) + 1
+                # add user to list of members
+                chatMembers.append(receivedMessage["username"])
+                seperateRooms[roomId] = chatMembers
+                # create log file 
+                f = open(f"SR_{roomId}_messagelog.txt", "w")
+                f.close()
+                # send confirmation
+                message = f'{{"type": "srb", "id": "{roomId}", "users": "{usernames}"}}'
+                self.clientSocket.send((message).encode())
+            
+            elif (receivedMessage['type'] == "SRM"):
+                user = receivedMessage["username"]
+                # check if room id exists 
+                roomId = int(receivedMessage['id'])
+                exists = False
+                userMember = False
+                for room, members in seperateRooms.items():
+                    print(members)
+                    print(user)
+                    if roomId == room:
+                        exists = True
+                        if user in members:
+                            userMember = True
+                if (not exists):
+                    message = f'{{"type": "invalidRoom"}}'
+                    self.clientSocket.send((message).encode())
+                    continue
+                # check username is member of the room 
+                if (not userMember):
+                    message = f'{{"type": "invalidMember"}}'
+                    self.clientSocket.send((message).encode())
+                    continue
+                # append to corresponding message log file
+                # send confirmation 
         self.clientSocket.close()
 
 if __name__ == '__main__':
@@ -162,9 +275,8 @@ if __name__ == '__main__':
         exit()
     serverPort = int(sys.argv[1])
     numFails = int(sys.argv[2])
-    if (1 > numFails and numFails > 5):    
+    if (1 > numFails or numFails > 5):    
         print("Has to be an integer between 1 and 5")
-        print(sys.argv[2])
         exit()
     serverSocket = socket(AF_INET, SOCK_STREAM)
     serverSocket.bind(('localhost', serverPort))
